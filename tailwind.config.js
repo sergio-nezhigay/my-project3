5b64dad0caebdf1674df39d544c2901d0050efbd
Sergio Nezhigay <abc1971h@gmail.com>
2023-08-31 14:30:35 +0300

    add page2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false,
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        primary: "#FF0000",
-        secondary: "#00FF00",
-      },
-      fontFamily: {
-        sans: ["Roboto", "Arial", "sans-serif"],
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
