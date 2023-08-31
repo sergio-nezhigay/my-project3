260a05f2a1bac9cd46ae43f20a4f50e2d058fc08
Sergio Nezhigay <abc1971h@gmail.com>
2023-08-31 15:16:51 +0300

    sdf

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-import Head from "next/head";
+// import Head from "next/head";
 import "./globals.css";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata = {
 //   title: 'Create Next App',
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         />
         <meta name="next-size-adjust" content="try" key="desc" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
