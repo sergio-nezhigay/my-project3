9e1c8335e80cbca75e2f271e7b695a638377a9f0
Sergio Nezhigay <abc1971h@gmail.com>
2023-08-31 14:43:06 +0300

    add second

--- pages/page2.js
+++
@@ -1,7 +0,0 @@
-export default function page2() {
-  return (
-    <body className="bg-blue-300">
-      <h1 className="text-red-900">Hello2 Tailwind CSS</h1>
-    </body>
-  );
-}
---
+++ src/app/second/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <body className="bg-blue-300">
+      <h1 className="text-red-900">Second CSS</h1>
+    </body>
+  );
+}
