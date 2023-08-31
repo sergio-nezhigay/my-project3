4bab418b3c0d917d20f4abaf6045c711e79f91ec
Sergio Nezhigay <abc1971h@gmail.com>
2023-08-31 15:13:33 +0300

    correct

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,39 @@
-export const metadata = {
-  title: "Acme1",
-  openGraph: {
-    title: "Acme",
-    description: "Acme1 is a...",
-    "next-size-adjust": "Acme2 is a...",
-  },
-};
+import Head from "next/head";
 
-export default function Home() {
+function IndexPage() {
   return (
-    <body className="bg-blue-300">
-      <h1 className="text-red-900">Hello Tailwind CSS</h1>
-    </body>
+    <div>
+      <Head>
+        <title>
+          iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple
+        </title>
+        <meta
+          name="description"
+          content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+          key="desc"
+        />
+      </Head>
+      <h1>iPhones for Sale</h1>
+      <p>insert a list of iPhones1 for sale.</p>
+    </div>
   );
 }
+
+export default IndexPage;
+
+// export const metadata = {
+//   title: "Acme3",
+//   openGraph: {
+//     title: "Acme",
+//     description: "Acme1 is a...",
+//     nextSizeAdjust: "Acme2 is a...",
+//   },
+// };
+
+// export default function Home() {
+//   return (
+//     <body className="bg-blue-300">
+//       <h1 className="text-red-900">Hello Tailwind1 CSS</h1>
+//     </body>
+//   );
+// }
