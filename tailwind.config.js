effac3b5a5543e68cbca28fe31e95f253147198b
Sergio Nezhigay <abc1971h@gmail.com>
2023-08-31 14:04:45 +0300

    config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#FF0000",
+        secondary: "#00FF00",
+      },
+      fontFamily: {
+        sans: ["Roboto", "Arial", "sans-serif"],
       },
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
