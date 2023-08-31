e0c0a4ff151b26c17ce0db3207161b39559a887e
Sergio Nezhigay <abc1971h@gmail.com>
2023-08-31 14:51:38 +0300

    try1

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
+export const metadata = {
+  title: "Acme1",
+  openGraph: {
+    title: "Acme",
+    description: "Acme1 is a...",
+    "next-size-adjust": "Acme2 is a...",
+  },
+};
+
 export default function Home() {
   return (
     <body className="bg-blue-300">
